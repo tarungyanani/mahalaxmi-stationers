16cbfed6d9afff260cb6b4a0a75f2400744c8988
muskangyanani <muskaann.129@gmail.com>
2025-07-16 14:28:25 +0530

    Added product images in static folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,8 +71,7 @@ USE_TZ = True
 
 # ✅ Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
