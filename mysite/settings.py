170e41d5995996e22370f934738f23f04ecbc919
muskangyanani <muskaann.129@gmail.com>
2025-07-16 14:30:25 +0530

    Set STATIC_ROOT and collected static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,6 +72,8 @@ USE_TZ = True
 # ✅ Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
